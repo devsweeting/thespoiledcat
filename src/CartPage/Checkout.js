a73d29acca186a6963d7ff1224b6b67c8be55937
Devin Sweeting <devinsweeting@gmail.com>
2019-03-08 09:22:49 -0800

    research chechout page and firebase database

--- src/CartPage/Checkout.js
+++ src/CartPage/Checkout.js
@@ -6,6 +6,15 @@ function Checkout(props) {
   return(
     <React.Fragment>
       <h2> Checkout </h2>
+      
+      <p> First Name <p>
+      <p> Last Name <p>
+      <p> Address Form </p>
+      <p> City <p>
+      <p> State <p>
+      <p> ZipCode <p>
+      <button> Confirm Purchase </button>
+      
     </React.Fragment>
   )
 }
