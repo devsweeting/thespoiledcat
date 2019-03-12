ac0e7993b991c4c0656c5860fb2088d6fb6f05e8
Devin Sweeting <devinsweeting@gmail.com>
2019-03-12 08:55:46 -0700

    FINALLY implement scroll bar that hides on scroll down and shows on scroll up

--- src/Header.jsx
+++ src/Header.jsx
@@ -41,12 +41,44 @@ const StyledRoute = styled.section `
 
 
 class Header extends React.Component {
+  state = {
+    auth: false,
+    slide: 0,  // How much should the Navbar slide up or down
+    lastScrollY: 0,  // Keep track of current position in state
+  };
+
+  componentWillMount() {
+    // When this component mounts, begin listening for scroll changes
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    // If this component is unmounted, stop listening
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+   const { lastScrollY } = this.state;
+   const currentScrollY = window.scrollY;
+
+
+   if (currentScrollY > lastScrollY) {
+     this.setState({ slide: '-120px' });
+   } else {
+     this.setState({ slide: '0px' });
+   }
+   this.setState({ lastScrollY: currentScrollY });
+ };
 
 
   render() {
 
     return(
-      <NavWrapper>
+      <NavWrapper style={{
+          transform: `translate(0, ${this.state.slide})`,
+          transition: 'transform 90ms linear',
+        }}
+      >
         <Title>the Spoiled Cat</Title>
         <NavLinks>
           <StyledRoute><Link to="/cart"> View Cart </Link></StyledRoute>
